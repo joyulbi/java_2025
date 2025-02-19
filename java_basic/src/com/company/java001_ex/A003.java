48ff86ac7be5fd743adc3f9901be0c11b74df9da
yulbi Jo <urby0725@naver.com>
2025-02-19 15:26:53 +0900

    2025

--- java_basic/src/com/company/java001_ex/A003.java
+++ java_basic/src/com/company/java001_ex/A003.java
@@ -5,7 +5,6 @@ public class A003 {
 		System.out.println("A");
 		System.out.println("AB");
 		System.out.println("ABC");
-		System.out.printf("내나이%d,내이름은%s,좋아하는 숫자 파이 %.1f",70,"조율비",2.5);
 		
 		System.out.println("A\nAB\nABC");
 	}
